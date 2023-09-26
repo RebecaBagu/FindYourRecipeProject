7152920b6a189fe00eb0ead12cc352c7674ef9a8
RebecaBagu <rebecarisciuc@yahoo.com>
2023-09-26 20:58:13 +0300

    homePage2

--- FindYourRecipe/FindYourRecipe.Web/Services/APIService.cs
+++ FindYourRecipe/FindYourRecipe.Web/Services/APIService.cs
@@ -1,44 +1,42 @@
 ﻿using System;
 using System.Text;
-using FindYourRecipe.Contracts.Models;
 using Newtonsoft.Json;
 
 namespace FindYourRecipe.Web.Services
 {
-	public abstract class APIService
+	public class APiService
 	{
-		
 		HttpClient _client;
-		public APIService(HttpClient client)
+		public APiService(HttpClient client)
 		{
 			_client = client;
 		}
 
-		public async virtual Task<T> GetAsync<T>(string route)
-		{
-			var result = await _client.GetAsync(route);
-			result.EnsureSuccessStatusCode();
-			var json=await result.Content.ReadAsStringAsync();
-			return JsonConvert.DeserializeObject<T>(json);
-        }
-
-		public async virtual Task DeleteAsync(string route)
+		public async Task DeleteAsync(string route)
 		{
 			var result = await _client.DeleteAsync(route);
 			result.EnsureSuccessStatusCode();
 		}
 
-		public async virtual Task<T> PostAsync<T>(string route, object request)
+		public async Task<T> GetAsync<T>(string route)
+		{
+			var result = await _client.GetAsync(route);
+			result.EnsureSuccessStatusCode();
+			var json = await result.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<T>(json);
+        }
+
+		public async Task<T> PostAsync<T>(string route, object request)
 		{
 			var json = JsonConvert.SerializeObject(request);
 			var data = new StringContent(json, Encoding.UTF8, "application/json");
 			var result = await _client.PostAsync(route, data);
 			result.EnsureSuccessStatusCode();
 			var jsonResult= await result.Content.ReadAsStringAsync();
-			return JsonConvert.DeserializeObject<T>(jsonResult);
-		}
+            return JsonConvert.DeserializeObject<T>(jsonResult);
+        }
 
-        public async virtual Task<T> PutAsync<T>(string route, object request)
+        public async Task<T> PutAsync<T>(string route, object request)
         {
             var json = JsonConvert.SerializeObject(request);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
@@ -47,8 +45,6 @@ namespace FindYourRecipe.Web.Services
             var jsonResult = await result.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(jsonResult);
         }
-
-
     }
 }
 
--- FindYourRecipe/FindYourRecipe.Web/wwwroot/app.js
+++ FindYourRecipe/FindYourRecipe.Web/wwwroot/app.js
@@ -1,15 +1,17 @@
-﻿function InitializeSearchBar() {
+﻿
+function InitializeSearchBar () {
     $('#SearchBar').select2({ placeholder: "Select ingredients" });
 }
 
-//https://learn.microsoft.com/en-us/answers/questions/1188191/how-to-implement-(select-multiple)-in-blazor-serve
 function GetSearchBarValues() {
-    var searchBar = $('#SearchBar')[0];
     var results = [];
-    for (var i = 0; i < searchBar.options.length; i++) {
-        if (searchBar.options[i].selected) {
-            results[results.length] = parseInt(searchBar.options[i].value);
+    var sel = $('#SearchBar')[0];
+    for (var i = 0; i < sel.options.length; i++) {
+        if (sel.options[i].selected) {
+            results[results.length] = parseInt(sel.options[i].value);
         }
     }
     return results;
 };
+
+
